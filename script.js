6f5a17109595fc001ae9ab4a2be122c1af84a693
AchmadSyihabuddin234 <ahmadhardi525@gmail.com>
2025-10-16 20:08:14 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const tombolSapa = document.getElementById('sapaButton');
 
 tombolSapa.addEventListener('click', function () {
-  alert('Halo! Terima kasih sudah berkunjung');
+  alert('Tengateh Mon Atemmoh Engkok lee!');
 });
